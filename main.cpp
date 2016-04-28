6a5fb2e1bf9d11b8f9f98fa34cf232bfb4a9c189
Jason Nie <niemquan@126.com>
2016-04-28 13:54:04 +0800

    add a class of peron in the main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 
 using namespace std;
 
+class person{
+	
+	private string name;
+	public string GetName(){
+		return name;
+	}
+	
+	public void SetName(string inputName){
+		name = inputName;
+	}
+}
+
+
 int main(){
 	cout<<"Hello world"<<endl;
 	return 0;
 }
+
+
 
 
